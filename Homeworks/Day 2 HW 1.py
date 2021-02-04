363353918113488bdbfcdae2732f3de22df5137f
AM2109 <77005728+AM2109@users.noreply.github.com>
2021-02-04 13:33:55 +0530

    Add files via upload

---
+++ Homeworks/Day 2 HW 1.py
@@ -0,0 +1,16 @@
+#Asking the user for the length of the list
+length = int(input("Enter length of list"))
+length_for_while_loop = length
+list1 = []
+#Getting the user to input the elements of the list
+print("Enter elements of list")
+while length_for_while_loop != 0:
+  new_element = input()
+  list1.append(new_element)
+  length_for_while_loop -= 1
+print("The list that you entered is:")
+print(list1)
+middle = length//2
+print("The list after swapping the first half and the second half:")
+newlist = list1[middle:length] + list1[0:middle]
+print(newlist)
